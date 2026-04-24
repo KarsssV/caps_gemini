3f455b10ff934b3b0ea29a299d769fa6f6dc9f9f
KarsssV <163622473+KarsssV@users.noreply.github.com>
2026-04-24 12:30:58 +0700

    Revert "add frontend changes"
    This reverts commit 2b613cb635b99ea52028c3d1f31ff293fd3cd9d4.

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import AuthShowcase from "../../components/auth-showcase";
-import { AuthProvider } from "../../contexts/auth-context";
-
-export default function LoginPage() {
-  return (
-    <AuthShowcase variant="login" />
-  );
-}
